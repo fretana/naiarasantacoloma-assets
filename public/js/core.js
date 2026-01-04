138ce2f64d9eb4a4455dc1521715d9eeffb9bd48
Felix Retana <10095737+fretana@users.noreply.github.com>
2026-01-04 14:30:35 +0100

    .

--- public/js/core.js
+++ public/js/core.js
@@ -1,10 +1,2 @@
 
 
-
-import { initAccordions } from "./modules/accordion.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  if (document.querySelector(".my-accordion")) {
-    initAccordions();
-  }
-});
